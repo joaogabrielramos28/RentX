3b70d31fd442cc945358ee2e4c2ddf88cc96ce5c
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-02-18 20:27:26 -0300

    feat: configuring global themes :sparkles:

--- App.tsx
+++ App.tsx
@@ -1,6 +1,33 @@
 import React from "react";
+import AppLoading from "expo-app-loading";
+import {
+  useFonts,
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from "@expo-google-fonts/archivo";
+import { Inter_400Regular, Inter_500Medium } from "@expo-google-fonts/inter";
+
 import { Home } from "./src/Screens/Home";
+import { ThemeProvider } from "styled-components";
+import theme from "./src/Styles/theme";
 
 export default function App() {
-  return <Home />;
+  const [fontsLoaded] = useFonts({
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+    Inter_400Regular,
+    Inter_500Medium,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
+  );
 }
