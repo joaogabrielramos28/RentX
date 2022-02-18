9034badfd23af541ee0e70906950488bcb58222e
joaogabrielramos28 <joaogabrielramos28@outlook.com>
2022-02-18 16:19:15 -0300

    feat: Home screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Home } from "./src/Screens/Home";
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello</Text>
-    </View>
-  );
+  return <Home />;
 }
